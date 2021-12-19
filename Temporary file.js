32dffb8681305a8c4fec6e43b718a14774d2c44e
CodyWhitt <cody.whitt@bryanuniversity.edu>
2021-12-19 00:51:54 -0600

    UNV-101

---
+++ Temporary file.js
@@ -0,0 +1,27 @@
+if (1== "1" ){
+    console.log ("loose")
+}
+    else {console.log ("strict")
+}
+
+console.log (" ")
+//just some spacing
+
+if (1=== "1" ){
+    console.log ("loose")
+}
+    else {console.log ("strict")
+}
+
+console.log (" ")
+//just some spacing
+
+x = 1
+
+if (1 === x){
+    console.log ("strict")
+}else if (1 == x){
+    console.log ("loose")
+}else{
+    console.log ("not equal at all")
+}
